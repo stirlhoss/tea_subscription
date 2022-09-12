a988fbe5d43ab248920300630236ae29290304b0
Stirling Hostetter <stirlhoss@proton.me>
2022-09-12 16:48:40 -0500

    Fixed error with schema

--- db/migrate/20220912195515_create_subscriptions.rb
+++ db/migrate/20220912195515_create_subscriptions.rb
@@ -2,7 +2,7 @@ class CreateSubscriptions < ActiveRecord::Migration[7.0]
   def change
     create_table :subscriptions do |t|
       t.text :title
-      t.integer :price
+      t.float :price
       t.text :status
       t.integer :frequency
 
