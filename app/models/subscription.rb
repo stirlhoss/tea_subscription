c3ad83e20538468ba9440e406ffedd78230c626a
Stirling Hostetter <stirlhoss@proton.me>
2022-09-12 16:29:21 -0500

    Add models for tables

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,2 +1,6 @@
 class Subscription < ApplicationRecord
+  has_many :customer_subscriptions
+  has_many :subscription_teas
+  has_many :customers, through: :customer_subscriptions
+  has_many :teas, through: :subscription_teas
 end
