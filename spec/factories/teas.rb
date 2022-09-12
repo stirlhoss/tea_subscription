c11defa1f2b1a5c425caf5142cba68134bd28309
Stirling Hostetter <stirlhoss@proton.me>
2022-09-12 16:31:39 -0500

    Add factory for tea

--- spec/factories/teas.rb
+++ spec/factories/teas.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :tea do
-    title { "MyText" }
-    description { "MyText" }
-    temperature { 1.5 }
-    brew_time { 1 }
+    title { Faker::Tea.variety }
+    description { Faker::Lorem.sentence(word_count: 10) }
+    temperature { Faker::Number.decimal(l_digits: 2, r_digits: 2) }
+    brew_time { Faker::Number.between(from: 180, to: 240) }
   end
 end
