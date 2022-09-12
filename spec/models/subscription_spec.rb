c7adffa8e8bfd72e016131257135603761bd20ab
Stirling Hostetter <stirlhoss@proton.me>
2022-09-12 16:31:02 -0500

    Add tests for relationships

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many :customer_subscriptions }
+  it { should have_many :subscription_teas }
+  it { should have_many(:customers).through :customer_subscriptions }
+  it { should have_many(:teas).through :subscription_teas }
 end
