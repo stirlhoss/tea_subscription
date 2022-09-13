04e2aa4c8ed6d057e5b0acf224c18860e4c7d0ad
Stirling Hostetter <stirlhoss@proton.me>
2022-09-12 19:38:34 -0500

    Test factories

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,4 +3,15 @@ require 'rails_helper'
 RSpec.describe Customer, type: :model do
   it { should have_many :customer_subscriptions }
   it { should have_many(:subscriptions).through :customer_subscriptions }
+
+  describe 'factories' do
+    it 'can make a customer with a factory' do
+      cust = create(:customer)
+
+      expect(cust.first_name).to be_a String
+      expect(cust.last_name).to be_a String
+      expect(cust.email).to be_a String
+      expect(cust.address).to be_a String
+    end
+  end
 end
--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -3,4 +3,15 @@ require 'rails_helper'
 RSpec.describe Tea, type: :model do
   it { should have_many :subscription_teas }
   it { should have_many(:subscriptions).through :subscription_teas }
+
+  describe 'factories' do
+    it 'can make a tea with a factory' do
+      tea = create(:tea)
+
+      expect(tea.title).to be_a String
+      expect(tea.description).to be_a String
+      expect(tea.temperature).to be_a Float
+      expect(tea.brew_time).to be_a Integer
+    end
+  end
 end
