78220aa4621c7cbdf9092e2b46e1b6ad2422551c
Stirling Hostetter <stirlhoss@proton.me>
2022-09-14 14:47:16 -0500

    Create controller method for creating subscription

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,5 +1,17 @@
 class Api::V1::SubscriptionsController < ApplicationController
 
   def create
+    sub = Subscription.new(subscription_params)
+    if sub.save
+      render json: SubscriptionSerializer.new(sub), status: :created
+    else
+      render json: { errors: { details: 'Subscription could not be created with this data.' } }, status: :bad_request
+    end
+  end
+
+  private
+
+  def subscription_params
+    params.permit(:title, :price, :status, :frequency, :quantity, :customer_id, :tea_id)
   end
 end
