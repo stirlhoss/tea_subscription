9a330f1fe52df8ba751f4b5c83e03a8862d2851b
Stirling Hostetter <stirlhoss@proton.me>
2022-09-14 16:04:48 -0500

    Add route for customer_subscriptions

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :subscriptions
+      resources :subscriptions, only: %i[create update]
+      resources :customer_subscriptions, only: :show
     end
   end
 end
