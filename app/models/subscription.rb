90df3feb804703f4e8bfe61707596dc0d225e39c
Stirling Hostetter <stirlhoss@proton.me>
2022-09-14 14:54:34 -0500

    Add validations to subscription model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,11 @@
 class Subscription < ApplicationRecord
   belongs_to :customer
   belongs_to :tea
+
+  validates_presence_of :title,
+                        :price,
+                        :status,
+                        :frequency,
+                        :customer_id,
+                        :tea_id
 end
